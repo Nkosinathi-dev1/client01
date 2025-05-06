f2af73e1961f7521408682414c0f3809253ca39f
Nkosinathi-dev1 <nkosinathi.ngele.dev@gmail.com>
2025-05-06 23:36:42 +0200

    feat(logout): implement user logout by clearing the token

--- src/app/features/dashboard/admin-dashboard.component.ts
+++ src/app/features/dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'app/core/services/auth.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './admin-dashboard.component.css'
 })
 export class AdminDashboardComponent {
+  constructor(private authService: AuthService) {}
 
+  logout() {
+    this.authService.logout();
+  }
 }
